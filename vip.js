309ae5f8de0cfc9903f4d2f228176e92232a4d69
xioazeng <139104411+xioazeng@users.noreply.github.com>
2023-09-27 11:35:47 +0800

    vip.js

--- vip.js
+++ vip.js
@@ -3,15 +3,15 @@
 var body = $response.body;
 var obj = JSON.parse (body);
 
-obj.status = "ACTIVE"
+obj.status = ACTIVE;
 obj.canGetTrial = false;
 obj.startDate = 2023-09-20T04:02:55.187+0000;
-obj.subscription."type" = "PREMIUM_PLUS";
-obj.subscription."offlineGracePeriod" = 999999;
-obj.paymentType = "AmEx_CREDIT_CARD";
+obj.subscription.type = PREMIUM_PLUS;
+obj.subscription.offlineGracePeriod = 999999;
+obj.paymentType = AmEx_CREDIT_CARD;
 obj.paymentOverdue = false;
-obj.highestSoundQuality = "HI_RES";
-obj.validUntil = "2099-09-20T04:02:55.187+0000";
+obj.highestSoundQuality = HI_RES;
+obj.validUntil = 2099-09-20T04:02:55.187+0000;
 
 body = JOSN stringify(obj);
 $done({body});
